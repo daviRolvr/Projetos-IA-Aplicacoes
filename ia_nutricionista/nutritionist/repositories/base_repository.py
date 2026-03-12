74678ab047a24203d3c19dcce8ea3e50872cc975
Davi emnauel <daviemanueloliveira7@gmail.com>
2026-03-12 01:09:28 -0300

    Adicionar comentário explicativo na classe BaseRepository para melhor compreensão do código

--- ia_nutricionista/nutritionist/repositories/base_repository.py
+++ ia_nutricionista/nutritionist/repositories/base_repository.py
@@ -1,6 +1,8 @@
 from tinydb import table
 from nutritionist.database.engine import Engine
 
+## BaseRepository é a classe base para todos os repositórios, fornecendo a conexão com o banco de dados e as operações básicas de CRUD.
+
 class BaseRepository(Engine):
     def get_table(self, table_name: str) -> table.Table:
         return self.db.table(table_name)
